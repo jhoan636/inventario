76081b4455547a06ab0da84d33d80dbad399abdf
jhoan636 <jhoan.63@hotmail.com>
2025-07-05 17:40:36 -0500

    Add Usuario entity with fields for user management

---
+++ src/main/java/com/nexos/inventario/model/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.nexos.inventario.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Integer edad;
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @Column(name = "fecha_ingreso", nullable = false)
+    private LocalDate fechaIngreso;
+}
