28e21fad226f542117fe0feb8d6826f83a84d32e
jhoan636 <jhoan.63@hotmail.com>
2025-07-06 10:07:36 -0500

    Add CargoRepository interface for role data access

---
+++ src/main/java/com/nexos/inventario/repository/CargoRepository.java
@@ -0,0 +1,11 @@
+package com.nexos.inventario.repository;
+
+import com.nexos.inventario.model.entity.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+    Optional<Cargo> findByNombre(String nombre);
+
+}
