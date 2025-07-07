93884bbd62ff9cab63518ad9a1439be019c553da
jhoan636 <jhoan.63@hotmail.com>
2025-07-07 00:03:56 -0500

    Remove unused methods from `UsuarioRepository` for cleaner and more focused repository interface.

--- src/main/java/com/nexos/inventario/repository/UsuarioRepository.java
+++ src/main/java/com/nexos/inventario/repository/UsuarioRepository.java
@@ -4,14 +4,6 @@ import com.nexos.inventario.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByNombre(String nombre);
-
-    List<Usuario> findByCargoNombre(String cargoNombre);
-
-    boolean existsByNombre(String nombre);
 }
